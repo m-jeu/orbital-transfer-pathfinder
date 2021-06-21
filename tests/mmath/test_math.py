e47d7eda88012c9e451ef25d2ee73f1072ccfee1
unknown <maartenjeu@gmail.com>
2021-06-22 01:59:52 +0200

    Add mmath unit-tests

---
+++ tests/mmath/test_math.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+import mmath.math
+
+
+class Test(TestCase):
+    def test_v_avg(self):
+        self.assertEqual(mmath.math.v_avg(10), 10,
+                         "v_avg() should be able to calculate average for 1 parameter.")
+
+        self.assertEqual(mmath.math.v_avg(5, 10, 15), 10,
+                         "v_avg() should be able to calculate average for variable amount of integers.")
+
+        self.assertAlmostEqual(mmath.math.v_avg(0.2, 0.3), 0.25,
+                               "v_avg() should be able to calculate average for floating-point numbers.")
+
+        self.assertEqual(mmath.math.v_avg(-10, -20), -15,
+                         "v_avg() should be able to calculate average for negative numbers.")
+
+    def test_cosine_rule(self):
+        self.assertAlmostEqual(mmath.math.cosine_rule(6.5, 9.4, 131), 14.51827859,
+                               msg="""cosine_rule() should be able to get triangle side length based on 2 other sides
+                               and angle measured in degrees.""")
+
+        self.assertNotEqual(mmath.math.cosine_rule(6.5, 9.4, 2.28638132), 14.51827859,
+                            msg="cosine_rule() should not measure angle_dif in radians.")
