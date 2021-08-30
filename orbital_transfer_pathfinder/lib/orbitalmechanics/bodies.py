c22b2a0fa12a0adf025afd925afb78a5a754f4d5
TheOGZer0 <maartenjeu@gmail.com>
2021-08-30 16:34:49 +0200

    Add TODO comment

--- orbital_transfer_pathfinder/lib/orbitalmechanics/bodies.py
+++ orbital_transfer_pathfinder/lib/orbitalmechanics/bodies.py
@@ -97,6 +97,7 @@ class CentralBodyInOrbit(CentralBody):
         return orbit.sm_axis * (1 - orbit.eccentricity) * \
                ((own_mass / (3 * orbit.central_body.mass)) ** (1 / 3))
 
+    # TODO(m-jeu): Use numpy.linspace?
     # TODO(m-jeu): This could be split into 2 methods/functions:
     # One that's more modular, that just computes several values between certain values.
     # One that's specific to the Central Body, that calls the first method with the right numbers.
