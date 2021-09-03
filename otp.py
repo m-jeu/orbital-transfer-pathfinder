c25ab9ac0a50288dabf9a3dd676b9b29f0c1d5dd
TheOGZer0 <maartenjeu@gmail.com>
2021-09-03 22:13:51 +0200

    Add imports to __init__.py files to allow total package import

--- otp.py
+++ otp.py
@@ -1,9 +1,4 @@
-import orbital_transfer_pathfinder.lib.orbitalmechanics.known_objects as known_objects
-import orbital_transfer_pathfinder.lib.orbitalmechanics.orbitcollections as orbitcollections
-import orbital_transfer_pathfinder.lib.orbitalmechanics.manoeuvres as manoeuvres
-import orbital_transfer_pathfinder.lib.shortpathfinding.custom_dijkstras_algorithm as custom_dijkstras_algorithm
-
-import orbital_transfer_pathfinder.lib.orbitalmechanics.visualization as visualization
+import orbital_transfer_pathfinder
 
 
 def pick_from_choices(choices: dict[str: object]) -> object:
@@ -25,16 +20,16 @@ def pick_from_choices(choices: dict[str: object]) -> object:
 if __name__ == "__main__":
     print("WARNING: Orbit generation around any other body then earth currently not implemented.")
     print("Pick a central body:")
-    central_body = pick_from_choices(known_objects.celestial_bodies)
+    central_body = pick_from_choices(orbital_transfer_pathfinder.celestial_bodies)
 
-    if central_body != known_objects.celestial_bodies["Earth"]:  # FIXME(m-jeu): Actually implement proper CLI
+    if central_body != orbital_transfer_pathfinder.celestial_bodies["Earth"]:  # FIXME(m-jeu): Actually implement proper CLI
         raise NotImplementedError
 
     print("Pick a start orbit:")
-    start_orbit = pick_from_choices(known_objects.known_orbits)
+    start_orbit = pick_from_choices(orbital_transfer_pathfinder.known_orbits)
 
     print("Pick a target orbit:")
-    target_orbit = pick_from_choices(known_objects.known_orbits)
+    target_orbit = pick_from_choices(orbital_transfer_pathfinder.known_orbits)
 
     print("Pick precision:")
     permutations_per_section = pick_from_choices({"Low": 5,  # For 8gb of ram.
@@ -42,24 +37,24 @@ if __name__ == "__main__":
 
     print("Configuring orbits & manoeuvres.")
 
-    orbits_collection = orbitcollections.OrbitCollection(central_body,
-                                                         [manoeuvres.ProRetroGradeManoeuvre,
-                                                          manoeuvres.InclinationChange,
-                                                          manoeuvres.InclinationAndProRetroGradeManoeuvre])
+    orbits_collection = orbital_transfer_pathfinder.OrbitCollection(central_body,
+                                                                    [orbital_transfer_pathfinder.ProRetroGradeManoeuvre,
+                                                                     orbital_transfer_pathfinder.InclinationChange,
+                                                                     orbital_transfer_pathfinder.InclinationAndProRetroGradeManoeuvre])
 
     orbits_collection.add_orbit(start_orbit)
     orbits_collection.add_orbit(target_orbit)
 
     orbits_collection.create_orbits(permutations_per_section,
-                                    [known_objects.earth.add_radius(150000),  # These numbers are specific to earth.
-                                     known_objects.earth.add_radius(20000000)],
+                                    [orbital_transfer_pathfinder.earth.add_radius(150000),  # These numbers are specific to earth.
+                                     orbital_transfer_pathfinder.earth.add_radius(20000000)],
                                     inclination_increment=5)
 
     orbits_collection.compute_all_manoeuvres(True)
 
     print("Looking for shortest path.")
 
-    graph = custom_dijkstras_algorithm.CDijkstraGraph(list(orbits_collection.orbits))
+    graph = orbital_transfer_pathfinder.CDijkstraGraph(list(orbits_collection.orbits))
 
     distance, path, nodes = graph.find_shortest_path(start_orbit, target_orbit, True)
 
@@ -72,6 +67,4 @@ if __name__ == "__main__":
         print(path[i])
     print(nodes[-1])
 
-    visualization.visualize_orbits(nodes)
-
-# Temporary comment for testing purposes, please ignore
+    orbital_transfer_pathfinder.visualize_orbits(nodes)
